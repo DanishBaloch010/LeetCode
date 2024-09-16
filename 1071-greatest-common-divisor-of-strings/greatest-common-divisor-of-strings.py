43f800981e3bc2a12c2ba9daae0cc468be4604d4
Danish Baloch <68731898+DanishBaloch010@users.noreply.github.com>
2024-09-16 23:31:33 +0500

    Time: 42 ms (23.58%) | Memory: 16.4 MB (83.43%) - LeetSync

--- 1071-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -2,9 +2,7 @@ class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         
         merge = []
-        # i,j = 0
-        i = 0
-        j = 0
+        i,j = 0,0
         if str1+str2 != str2+str1:
             return ''
 
