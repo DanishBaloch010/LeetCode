32f9e5506ead9aacb5c095577a3489a78a78399f
Muhammad Danish <68731898+DanishBaloch010@users.noreply.github.com>
2024-09-19 23:36:47 +0500

    Time: 109 ms (31.14%) | Memory: 17.7 MB (40.64%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -4,10 +4,13 @@ class Solution:
         i, j = 0 , len(numbers)-1
 
         while True:
+            # if target found then return
             if numbers[i] + numbers[j] ==  target:
                 return [i +1 , j +1]
+            # if sum is greater become closer from left side to find a lesser number
             if numbers[i] + numbers[j] > target:
                 j = j - 1
+            # otherwise move forward from right side to find a bigger number 
             else:
                 i = i + 1
 
