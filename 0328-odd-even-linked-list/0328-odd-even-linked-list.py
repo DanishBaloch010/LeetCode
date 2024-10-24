72a32da89e078305568b81e4c09b2d88d466e565
Muhammad Danish <68731898+DanishBaloch010@users.noreply.github.com>
2024-10-24 12:30:34 +0500

    Time: 0 ms (100%), Space: 18.1 MB (97.59%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,31 +5,40 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
+        
+        # head nahi hai ya ek node hai.
         if not head or not head.next:
             return head
         
+        # odd pehla wala hai even uske agla wala (starting case)
         odd = head
-        even = even_head = head.next
+        even = head.next
+
+        # yaha par ek aur kaam hoga, wait for it.(done)
+        even_head =  head.next
 
         while even and even.next:
+            # odd ko ikhata karo
+            odd.next  = even.next
+            odd  = even.next
 
-            odd.next = even.next
-            odd = even.next
-
-            even.next = odd.next
+            # even ko ikhata karo
+            even.next =  odd.next
             even = odd.next
 
+            # just for understanding
+            # current = current.next
+
         odd.next = even_head
+
         return head
-            
-
-        
 
 
 
 
-            
+
+
+
 
 
         
