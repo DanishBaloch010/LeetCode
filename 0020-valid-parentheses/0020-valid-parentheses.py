99bf3b0efe4f04592e061db8e2f1a79c200cbed7
Muhammad Danish <68731898+DanishBaloch010@users.noreply.github.com>
2024-10-30 11:31:28 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) < 2:
+            return False
+
+        brackets = {')':'(', '}':'{', ']':'['}
+
+        stack = []
+        for char in s:
+            # closing bracket
+            if char in brackets:
+                top = "#"
+                if stack:
+                    top = stack.pop()
+                    if top != brackets[char]:
+                        return False
+            else:
+                stack.append(char)
+
+        print(stack)
+        return not stack
