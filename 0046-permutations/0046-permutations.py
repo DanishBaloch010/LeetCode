69e77f3f5fb78256162819a731526cc5de736fb0
Muhammad Danish <68731898+DanishBaloch010@users.noreply.github.com>
2026-01-25 22:22:16 -0500

    Time: 1 ms (47.54%), Space: 19.7 MB (12.34%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        ans=[]
+
+        subset=[]
+
+        def helper(arr):
+            #base case
+            if len(arr)==len(nums):
+                ans.append(arr[:])
+                return
+            else:
+                for i in range(len(nums)): # this is the correct one instead of for i in range(len(arr))
+                    if nums[i] not in arr:
+                        arr.append(nums[i])
+                        helper(arr)
+                        arr.pop()
+        helper(subset)
+        return ans
+        
