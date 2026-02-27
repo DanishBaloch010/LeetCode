fa05735c183907d308b3fc5e13d2e302029981ab
Muhammad Danish <68731898+DanishBaloch010@users.noreply.github.com>
2026-02-27 14:27:26 -0500

    Time: 0 ms (100%), Space: 19.2 MB (78.18%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -7,18 +7,44 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
+
+
         # traverse the root in pre-order, in-order and post-order while inverting and see if it does make any difference or not.
+
+        # # pre-order traversal
+        # if root:
+        #     # what you are about to swap with make sure you keep a copy for that in temp (nevermind, this is a just a note to myself)
+        #     temp = root.right
+        #     root.right =  root.left
+        #     root.left = temp
+            
+        #     self.invertTree(root.left)
+        #     self.invertTree(root.right)
+
+
+        # # in-order traversal
+        # if root:
+        #     # what you are about to swap with make sure you keep a copy for that in temp (nevermind, this is a just a note to myself)
+      
+            
+        #     self.invertTree(root.left)
+        #     temp = root.right
+        #     root.right =  root.left
+        #     root.left = temp
+        #     self.invertTree(root.right)        
+
+        # post-order traversal
         if root:
             # what you are about to swap with make sure you keep a copy for that in temp (nevermind, this is a just a note to myself)
+      
+            
+            self.invertTree(root.left)
+            self.invertTree(root.right)
             temp = root.right
             root.right =  root.left
             root.left = temp
             
-            # pre-order traversal
-            self.invertTree(root.left)
-            self.invertTree(root.right)
 
-        
         return root
 
     
