2858ed59edb4bca53058a11990548f533038881b
Muhammad Danish <68731898+DanishBaloch010@users.noreply.github.com>
2026-05-29 16:56:45 -0400

    0191-NUMBER-OF-1-BITS Stats: Time: 0 ms (100%), Space: 19.3 MB (21.74%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,23 +1,12 @@
-import numpy as np
 class Solution:
     def hammingWeight(self, n: int) -> int:
 
-        binary =  ""
+        set_bits = 0 
+        while n:
+            # add when right most bit & operation is 1 or 0, zero will have no effect on the addition.
+            set_bits += n & 1
+            # shift the bits from left to right.
+            n = n >> 1
 
-        while n > 0:
-            binary = str(n%2) + binary
-            n = n // 2
-            
-        print(binary)
-        count = 0
-        for bit in binary:
-            if bit == '1':
-                count= count + 1
-
-        return count
-
-
-
-        
-        
+        return set_bits 
         
