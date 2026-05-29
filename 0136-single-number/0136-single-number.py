041cae1da1ac6dd28d774b4aebac7b70483ab765
Muhammad Danish <68731898+DanishBaloch010@users.noreply.github.com>
2026-05-29 12:39:58 -0400

    0136-SINGLE-NUMBER Stats: Time: 0 ms (100%), Space: 21 MB (93.41%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        
+        # the brute force method would be to count number of unique entities in a dictionary and then checking count > 1 in the dictionary.
 
         res = 0
-        for n in nums:
-            res = res ^ n 
-        
+        for num in nums:
+            res = res ^ num
         return res
-
-
-        
