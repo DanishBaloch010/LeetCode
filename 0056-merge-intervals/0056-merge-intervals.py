bbe12f69470aa4c47826c98bf2110250078d2013
Muhammad Danish <68731898+DanishBaloch010@users.noreply.github.com>
2026-06-29 16:45:11 -0400

    Time: 0 ms (100%), Space: 22.6 MB (50.42%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,29 +1,25 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        # sort the interval first based on the start value so that we know that they are line up in sequential increasing order based in start.
+        sorted_intervals_on_start = sorted(intervals , key = lambda x: x[0])
+        prev_interval = [sorted_intervals_on_start[0]]
 
-        sorted_intervals =  sorted(intervals, key=lambda x: x[0])
+        for idx, next_interval in enumerate(sorted_intervals_on_start , start =1):
 
-        print(sorted_intervals)
+                # if next interval start is smaller than or equals to the previous interval end then it means the next one inside the previous one
+                if next_interval[0] <= prev_interval[-1][1]:
+                    # update the max outer boundry
+                    prev_interval[-1][1] =  max(prev_interval[-1][1] , next_interval[1])
 
-        results = [sorted_intervals[0]]
-        for idx, next_interval in enumerate(sorted_intervals, start=1):
-            prev_interval = results[-1]
-            # if the start of the next is under the previous one then merge them.
-            if next_interval[0] <= prev_interval[1]:
-                # store the start and the end
-                results[-1][1] =  max(prev_interval[1], next_interval[1])
-            # no overlap, means the interval is distinct
-            else:
-                results.append(next_interval)
+                else:
+                    prev_interval.append(next_interval)
 
-
-        return results
-
-
+        return prev_interval
+                
+            
 
 
 
 
-
-        return [1,2]
         
